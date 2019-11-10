9063de9196e7f4939c125ee7c9526a1e21822ee8
Nate Ball <naball@calpoly.edu>
2019-11-10 11:41:05 -0800

    Back throw attempt

---
+++ Scenes/rolling.gd
@@ -0,0 +1,25 @@
+extends Sprite
+
+# Declare member variables here. Examples:
+var total_frames = 8
+var cur_frame = 0
+var animation_speed = 0.05
+var animation_timer = 0
+var cur_dir = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	animate(delta)
+
+func animate(delta):
+	animation_timer += delta
+	if animation_timer >= animation_speed:
+		cur_frame += 1
+		animation_timer = 0
+	if cur_frame >= total_frames:
+		cur_frame = 0
+	self.frame = cur_frame
--- Scripts/player1.gd
+++ Scripts/player1.gd
@@ -28,24 +28,47 @@ var attacking = 0
 
 #Attacking
 var attacking_cooldown = 0.3
+var punch_stun_timer = 0.6
+var throw_stun_timer = 5
+var stun_timer = 0
+var am_hit = 0
+var things_hit
+var other_player
+var am_rolling = false
+var am_thrown = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	other_player = self.get_parent().find_node("player2")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move_player(delta)
 
+func animate(animation):
+	self.find_node("running").visible = false
+	self.find_node("standing").visible = false
+	self.find_node("punching").visible = false
+	self.find_node("hurt").visible = false
+	self.find_node("rolling").visible = false
+	self.find_node("thrown").visible = false
+	self.find_node(animation).visible = true
+	self.find_node(animation).flip_h = dir
+	
 # Controls inputs and calls physics commands
 func move_player(delta):
-	if !attacking:
-		control_right()
-		control_left()
-		control_move()
-	control_jumping()
-	control_falling()
-	control_attacking(delta)
+	if !am_hit && !am_rolling && !am_thrown:
+		if !attacking:
+			control_right()
+			control_left()
+			control_move()
+		control_jumping()
+		control_attacking(delta)
+	if !am_rolling && !am_thrown:
+		control_falling()
+	control_hurting(delta)
+	if self.find_node("rolling").cur_frame == self.find_node("rolling").total_frames - 1:
+		am_rolling = false
 
 func control_right():
 	moving_right = false
@@ -65,18 +88,12 @@ func control_left():
 
 func control_move():
 	if (moving_right && moving_left) || (!moving_left && !moving_right):
-		self.find_node("running").visible = false
-		self.find_node("standing").flip_h = dir
-		self.find_node("standing").visible = true
-		self.find_node("punching").visible = false
+		animate("standing")
 	else:
-		self.find_node("running").visible = true
-		self.find_node("standing").visible = false
-		self.find_node("punching").visible = false
-		self.find_node("running").flip_h = dir
+		animate("running")
 
 func control_jumping():
-	if Input.is_key_pressed(KEY_W) && grounded:
+	if Input.is_key_pressed(KEY_W) && grounded && !attacking:
 		jumping += 1
 		falling = 0
 		grounded = false
@@ -104,11 +121,56 @@ func control_falling():
 		falling = 1
 	if !grounded && !falling && !jumping:
 		falling += 1
+
+func get_hit(which_attack):
+	am_hit = which_attack
+	self.find_node("thrown").cur_frame = 0
+
+func control_punched():
+	animate("hurt")
+	if stun_timer > punch_stun_timer:
+		am_hit = 0
+		stun_timer = 0
+
+func control_thrown():
+	animate("thrown")
+	self.global_position = other_player.global_position
+	if stun_timer > throw_stun_timer:
+		self.find_node("hurt_box").disabled = false
+		am_thrown = false
+		am_hit = 0
+		stun_timer = 0
+
+func control_hurting(delta):
+	stun_timer += delta
+	if am_hit == 1:
+		control_punched()
+	elif am_hit == 2:
+		am_thrown = true
+		self.find_node("hurt_box").disabled = true
+		control_thrown()
+	
+
+func hit_opponent(which_attack):
+	other_player.get_hit(which_attack)
+	
+
 func control_attacking(delta):
 	if Input.is_key_pressed(KEY_SPACE) && !attacking:
 		attacking = 1
 		attacking_timer = 0
+		self.find_node("fist").find_node("fist_box").disabled = false
 	if attacking:
+		animate("punching")
+		things_hit = self.find_node("fist").get_overlapping_bodies()
+		if things_hit.find(other_player) != -1:
+			if other_player.dir == self.dir:
+				hit_opponent(2)
+				self.animate("rolling")
+				am_rolling = true
+			else:
+				hit_opponent(1)
+			self.find_node("fist").find_node("fist_box").disabled = true
 		attacking_timer += delta
 		if attacking_timer < attack_time * 0.5:
 			self.find_node("punching").frame = 0
@@ -117,7 +179,5 @@ func control_attacking(delta):
 		if attacking_timer > attack_time:
 			attacking = 0
 			attacking_timer = 0
-		self.find_node("running").visible = false
-		self.find_node("standing").visible = false
-		self.find_node("punching").visible = true
-		self.find_node("punching").flip_h = dir
+	else:
+		self.find_node("fist").find_node("fist_box").disabled = true
